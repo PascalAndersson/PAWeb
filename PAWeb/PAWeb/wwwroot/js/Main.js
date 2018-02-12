e399fe058fd55b18120e69af1553515affb2c908
Pascal Andersson <anderssonpascal@gmail.com>
2018-02-12 23:10:05 +0100

    Initial commit

---
+++ PAWeb/PAWeb/wwwroot/js/Main.js
@@ -0,0 +1,26 @@
+﻿
+
+$(function () {
+    $("#navBar li").click(function () {
+        var idToScroll = checkNavbarId(this.id);
+        scrollToId(idToScroll);
+    });
+});
+
+function checkNavbarId(id) {
+    var clickedId = "";
+
+    if (id == "about" || id == "pageTitleName")
+        clickedId = "#aboutPage";
+    else if (id == "projects")
+        clickedId = "#projectsPage";
+    else if (id == "contact")
+        clickedId = "#contactPage";
+    return clickedId;
+}
+
+function scrollToId(id) {
+    $('html, body').animate({
+        scrollTop: $(id).offset().top
+    }, 1000);
+}
