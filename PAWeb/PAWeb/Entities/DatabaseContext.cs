8ea2845445c4e942b5e73a02487045f3b804d793
Pascal Andersson <anderssonpascal@gmail.com>
2018-02-15 12:30:35 +0100

    Added Db to project

---
+++ PAWeb/PAWeb/Entities/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PAWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PAWeb.Entities
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> context) : base(context)
+        {
+
+        }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
---
+++ PAWeb/PAWeb/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PAWeb.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
