70d22813728c47364863e2f730c301f9773ebeed
Pascal Andersson <anderssonpascal@gmail.com>
2018-02-15 16:22:34 +0100

    Backend for adding projects
    ^ and generate html for projects in project.js

--- PAWeb/PAWeb/Models/Project.cs
+++ PAWeb/PAWeb/Models/Project.cs
@@ -11,6 +11,6 @@ namespace PAWeb.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Image { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
---
+++ PAWeb/PAWeb/wwwroot/js/Project.js
@@ -0,0 +1,48 @@
+﻿$(function () {
+    getAllProjects();
+});
+
+function getAllProjects() {
+    $.ajax({
+        url: "api/home/getAllProjects",
+        method: "GET"
+    })
+        .done(function (allProjects) {
+            var html = "";
+            allProjects.forEach(function (project) {
+                html += displayAllProjects(project);
+            });
+            $("#projectsContainer").html(html);
+        })
+        .fail(function (xhr, status, error) {
+            console.log(xhr, status, error);
+        });
+}
+
+function displayAllProjects(project) {
+    var image = checkIfImageUrlIsNull(project.imageUrl);
+
+    var html = '<div class="row projectWrapper">';
+    html += '<div class="col-md-7">';
+    html += '<p class="projectTitle">' + project.title + '.</p>';
+    html += '<p class="projectDescription">' + project.description + "</p>";
+    html += '</div>';
+    html += '<div class="col-md-4 projectImage">' + image + '</div>';
+    html += '</div>';
+
+    return html;
+}
+
+function checkIfImageUrlIsNull(image) {
+    //console.log(image);
+    var imageUrl = "";
+
+    if (imageUrl !== null) {
+        console.log("img url is not null");
+        return imageUrl;
+    }
+    else {
+        console.log("imageUrl is null...");
+        imageUrl = "imageUrl is null...";
+    }
+}
